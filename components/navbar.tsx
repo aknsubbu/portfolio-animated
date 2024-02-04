7bf3fedaa53f115bd64414723781bce414e56c6f
aknsubbu <aknsubbu@gmail.com>
2024-02-04 22:09:33 +0530

    Update navbar and site config

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -117,7 +117,7 @@ export const Navbar = () => {
 										? "danger"
 										: "foreground"
 								}
-								href="#"
+								href={item.href}
 								size="lg"
 							>
 								{item.label}
